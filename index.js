1c8cbd75042c6635240d9946bdc27c5492b56040
Robert Jackson <robert.w.jackson@me.com>
2014-05-12 23:57:29 -0400

    Fix typo.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function WatchedTree (inputTree) {
   this.description = 'Watched - ' + inputTree;
 }
 
-UnwatchedTree.prototype.read = function (readTree) { 
+WatchedTree.prototype.read = function (readTree) {
   return readTree(this.inputTree)
 };
-UnwatchedTree.prototype.cleanup = function () { };
+WatchedTree.prototype.cleanup = function () { };
 
 module.exports = WatchedTree;
