7a55d7f8f7f4ba6846693373a280ef5d0ea838e7
houfeng <houfeng0923@gmail.com>
2020-05-01 06:53:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let Plugin = require('broccoli-plugin');
+var Plugin = require('broccoli-plugin');
 
 WatchedTree.prototype = Object.create(Plugin.prototype);
 WatchedTree.prototype.constructor = WatchedTree;
 function WatchedTree(inputNodes, options) {
   options = options || {};
   Plugin.call(this, inputNodes, {
+    persistentOutput: true,
+    needsCache: false,
     annotation: options.annotation || ('Watched - ' + inputNodes)
   });
 }
